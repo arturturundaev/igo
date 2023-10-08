856bc884bc6e7afaf318abc33f8fa725cca046b8
Artur Turundaev <a.turundaev@etpgpb.ru>
2023-10-08 23:11:57 +0300

    add Viper to read config

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 docker-compose up -d
 
 ### Architecture
+
+
+# TODO STEP BY STEP
+1. Install Viper (github.com/spf13/viper). 
+go get github.com/spf13/viper
+Viper - lib to read special technical files with config
+
+AddConfigPath - add path to config files
+InitConfig - name of config file
--- cmd/core/main.go
+++ cmd/core/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// 1. Load config variables
+	viper.AddConfigPath("config")
+	viper.SetConfigName("test")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	port := viper.GetString("server.port")
+
+	// 2. Declare API methods and handlers
 	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = fmt.Fprintf(w, "Hello, World!2345")
 	})
-	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+
+	fmt.Printf("Server running (port=%s), route: http://localhost:8080/helloworld\n", port)
+
+	// 3. Start server
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+
 }
